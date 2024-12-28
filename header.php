893ef675e0ecea6b86256b91d770347adc15aabd
unknown <fachinformatik1@gmail.com>
2024-12-28 16:53:09 +0100

    header changes

--- header.php
+++ header.php
@@ -1,9 +1,14 @@
 <!DOCTYPE html>
-<html>
+<html <?php language_attributes(); ?>>
+
+<!-- word Press-Header -->
   <head>
+     <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
   </head>
-  <body>
+
+  <body <?php body_class(); ?> >
     <header class="site-header">
       <div class="container">
         <h1 class="school-logo-text float-left">
